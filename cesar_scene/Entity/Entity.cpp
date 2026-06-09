ed7c3175c14747744a3e2a37400776cad8f935ec
Ammishaddai Boakye Yiadom <ammishaddai760@gmail.com>
2026-06-09 16:24:09 +0000

    Fixed AddChildEntityBug. The function was not correctly updating child entities and was setting the Relationship comp of the parent to that of it's child

--- cesar_scene/Entity/Entity.cpp
+++ cesar_scene/Entity/Entity.cpp
@@ -2,19 +2,17 @@
 #include "Component.h"
 
 namespace cesar {
-
 	std::string_view Entity::GetTag() {
 		CESAR_ASSERT(HasComponent<TagComponent>() && "Entity must has a tag component");
 		return registery->get<TagComponent>(entity).tag;
 	}
-
 	void Entity::AddChildEntity(Entity& entt) {
 		CESAR_ASSERT(HasComponent<RelationshipComponent>() && "Entity must have a relationship component");
 		RelationshipComponent& relationship_component = registery->get<RelationshipComponent>(entity);
 		relationship_component.children.push_back(entt);
 
-		relationship_component = entt.GetComponent<RelationshipComponent>();
-		relationship_component.parent = entity;
+		RelationshipComponent& child_relationship_component = entt.GetComponent<RelationshipComponent>();
+		child_relationship_component.parent = entity;
 	}
 
 	const std::vector<entt::entity>& Entity::GetChildren()
